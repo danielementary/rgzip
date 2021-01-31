c54535b1a43b29adb38d3ea7ca06ebe628923ede
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2021-01-31 21:56:21 +0100

    readn and print bytes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
+    let bytes = fs::read(filename).expect("Something went wrong reading the file");
 
     println!("Compressing file {}", filename);
+
+    println!("With bytes:");
+    for byte in bytes.iter() {
+        println!("\t{:#010b}", byte);
+    }
 }
